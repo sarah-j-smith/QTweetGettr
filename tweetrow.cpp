3b58cb4d18e9f32afe5908a8dc27a29d4692c318
Sarah Smith <sarah@smithsoft.com.au>
2017-09-20 21:59:25 +1000

    Get tablewidget working

--- tweetrow.cpp
+++ tweetrow.cpp
@@ -12,3 +12,44 @@ TweetRow::~TweetRow()
 {
     delete ui;
 }
+
+QString TweetRow::statusText() const
+{
+    return ui->statusText->text();
+}
+
+void TweetRow::setStatusText(const QString &text)
+{
+    ui->statusText->setText(text);
+}
+
+QString TweetRow::twitterName() const
+{
+    return ui->userTitle->text();
+}
+
+void TweetRow::setTwitterName(const QString &name)
+{
+    ui->userTitle->setText(name);
+}
+
+QString TweetRow::twitterHandle() const
+{
+    return ui->userHandle->text();
+}
+
+void TweetRow::setTwitterHandle(const QString &handle)
+{
+    ui->userHandle->setText(handle);
+}
+
+QPixmap TweetRow::profilePicture() const
+{
+    auto pic = ui->userIcon->pixmap();
+    return QPixmap(*pic);
+}
+
+void TweetRow::setProfilePicture(const QPixmap &picture)
+{
+    ui->userIcon->setPixmap(picture);
+}
--- tweetrow.h
+++ tweetrow.h
@@ -2,6 +2,7 @@
 #define TWEETROW_H
 
 #include <QWidget>
+#include <QPixmap>
 #include <QTableWidgetItem>
 
 namespace Ui {
@@ -11,10 +12,28 @@ class TweetRow;
 class TweetRow : public QWidget
 {
     Q_OBJECT
-
+    Q_PROPERTY(QString statusText READ statusText WRITE setStatusText NOTIFY statusTextChanged);
+    Q_PROPERTY(QString twitterName READ twitterName WRITE setTwitterName NOTIFY twitterNameChanged);
+    Q_PROPERTY(QString twitterHandle READ twitterHandle WRITE setTwitterHandle NOTIFY twitterHandleChanged);
+    Q_PROPERTY(QPixmap profilePicture READ profilePicture WRITE setProfilePicture NOTIFY profilePictureChanged);
 public:
     explicit TweetRow(QWidget *parent = 0);
     ~TweetRow();
+
+    QString statusText() const;
+    void setStatusText(const QString &text);
+    QString twitterName() const;
+    void setTwitterName(const QString &name);
+    QString twitterHandle() const;
+    void setTwitterHandle(const QString &handle);
+    QPixmap profilePicture() const;
+    void setProfilePicture(const QPixmap &picture);
+
+signals:
+    void statusTextChanged(QString);
+    void twitterNameChanged(QString);
+    void twitterHandleChanged(QString);
+    void profilePictureChanged(QPixmap);
 
 private:
     Ui::TweetRow *ui;
